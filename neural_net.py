5f121b0f92cf05ca302058f85d4bcd51051287e5
Ian Smith <ianmsmith17@gmail.com>
2020-02-12 11:11:32 -0600

    added data collection and formatting

--- neural_net.py
+++ neural_net.py
@@ -4,3 +4,15 @@ from tensorflow import keras
 import numpy as np
 import matplotlib as plt
 
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(18, input_shape=('none')),
+  tf.keras.layers.Dense(20),
+  tf.keras.layers.Dense(20),
+  tf.keras.layers.Dense(2)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(),
+              metrics='accuracy')
+
